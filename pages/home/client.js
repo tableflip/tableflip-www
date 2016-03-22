590bd7315db769b7f83f24f7dc0f2a2313b01585
Bernard Mordan <bernardmordan@gmail.com>
2016-03-22 12:28:41 +0000

    adds sent messages to enquiry-form and startup-form

--- pages/home/client.js
+++ pages/home/client.js
@@ -1,10 +1,20 @@
 var photoGrid = require('../../lib/photos')
+var sentTpl = require('./sent.jade')
 
 module.exports = function (page) {
-  page('/', enter)
+  page('/', enter, sent)
 }
 
 function enter (ctx, next) {
+  if (ctx.querystring === 'sent=true') return next()
   var gridEl = document.getElementById('sketchpad')
   photoGrid(gridEl)
+}
+
+function sent (ctx, next) {
+  var el = document.getElementById('enquiry-form')
+  var top = el.getBoundingClientRect().top - 144
+  el.innerHTML = sentTpl()
+  window.scrollTo(0, top)
+  document.getElementById('sent-message').style.color = 'black'
 }

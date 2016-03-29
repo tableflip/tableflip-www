2da69de3fcf556ad0d4469836b77de006dce5b11
Oli Evans <oli@zilla.org.uk>
2016-03-29 14:29:48 +0100

    Pull sent guard into sent function

--- pages/home/client.js
+++ pages/home/client.js
@@ -2,16 +2,16 @@ var photoGrid = require('../../lib/photos')
 var sentTpl = require('./sent.jade')
 
 module.exports = function (page) {
-  page('/', enter, sent)
+  page('/', sent, enter)
 }
 
 function enter (ctx, next) {
-  if (ctx.querystring === 'sent=true') return next()
   var gridEl = document.getElementById('sketchpad')
   photoGrid(gridEl)
 }
 
 function sent (ctx, next) {
+  if (ctx.querystring !== 'sent=true') return next()
   var el = document.getElementById('enquiry-form')
   var top = el.getBoundingClientRect().top - 144
   el.innerHTML = sentTpl()
